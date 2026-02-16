189700ddc0f79281f9c6ef17a373e9a058bb3a1d
Rishu <rr2966@srmist.edu.in>
2026-02-16 21:14:40 +0530

    first commit

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * UC1: Print the literal text "OOPS" to the console.
+ * This is the foundation of the OOPS Banner App project.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Application entry point.
+     * Prints "OOPS" to the console as required by UC1.
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
